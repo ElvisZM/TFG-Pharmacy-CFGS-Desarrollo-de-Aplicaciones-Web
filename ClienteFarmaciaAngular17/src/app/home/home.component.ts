9378b6b888a6027e9953cc2a34a604b813d614a0
Elvis Zhune <elfabri28@gmail.com>
2024-04-23 18:11:43 +0200

    Terminar DoCheck username google

--- ClienteFarmaciaAngular17/src/app/home/home.component.ts
+++ ClienteFarmaciaAngular17/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, OnInit, DoCheck } from '@angular/core';
 import { TopVentasComponent } from '../top-ventas/top-ventas.component';
 import { TypewriterService} from '../servicios/typewriter.service';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
-import { map } from 'rxjs';
+import { isEmpty, map } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { AuthService } from '../servicios/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,15 +15,39 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
-
-  titles = [', .', ', hoy y siempre.', ', cada paso del camino.'];
-
+export class HomeComponent implements OnInit{
+  
   private typewriterService = inject(TypewriterService);
+  
+  username!: string;
+  userPicture!: string;
+  typedText$!: any;
+  titles!: Array<string>;
 
-  typedText$ = this.typewriterService
+  constructor(private authService: AuthService){ }
+
+  ngOnInit(): void {  
+    this.usernameTitleAnimated();
+  }
+
+  usernameTitleAnimated(){
+    this.titles = [', hoy y siempre.', ', cada paso del camino.']
+    if (this.checkUsernameGoogle()!= undefined){
+      this.titles = [`, ${this.username}`,', hoy y siempre.', ', cada paso del camino.']
+    }
+    this.typedText$ = this.typewriterService
     .getTypewriterEffect(this.titles)
     .pipe(map((text) => text));
+    
+  }
+  checkUsernameGoogle():string | undefined {  
+    if(this.authService.getGoogleUserCookie().length > 0){
+      this.username = JSON.parse(this.authService.getGoogleUserCookie()).given_name;
+      return this.username;
+    }
+    return undefined;
+  }
+
 
 }
 
